902b77c52fbf545ee2003b2db26f9c0427480d94
Paulo Victor <paulovictorbezerrabrito23@gmail.com>
2024-11-24 22:52:43 -0300

    cliente.py

--- Projeto_1/projeto_1 - Paulo Victor/src/cliente.py
+++ Projeto_1/projeto_1 - Paulo Victor/src/cliente.py
@@ -1,24 +1,14 @@
 class Cliente:
     def __init__(self, nome, idade, saldo):
-        """
-        Inicializa uma nova instância da classe Cliente.
-        :param nome: Nome do cliente (str).
-        :param idade: Idade do cliente (int).
-        :param saldo: Saldo do cliente (float).
-        """
+
         self.__nome = nome
         self.__idade = idade  
         self.__saldo = saldo  
 
     def mostrar_informacoes(self):
-        """
-        Exibe as informações do cliente.
-        """
+    
         print(f"Cliente: {self.__nome}, Idade: {self.__idade}, Saldo: {self.__saldo:.2f}")
 
     def atualizar_saldo(self, valor):
-        """
-        Atualiza o saldo do cliente.
-        :param valor: Valor a ser adicionado ao saldo (pode ser negativo).
-        """
+        
         self.__saldo += valor
