d93eb87018f6f5372b93653d5ea9bbddb608695e
Taíssa Moreira <taissamoreira403@gmail.com>
2024-12-09 22:39:09 -0300

    Update teste.py

--- Projeto_1/teste/teste.py
+++ Projeto_1/teste/teste.py
@@ -1,3 +1,9 @@
+# Avisa para o python onde localizar o arquivo do cliente.py:
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 # Exemplo de uso:
 
 from cliente import Cliente
