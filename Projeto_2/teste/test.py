c910ae0528503608289292a1430dfd4915b606dd
Weslem <91810375+wes-lem@users.noreply.github.com>
2024-12-11 20:52:52 -0300

    sys.path corrigido diretório

--- Projeto_2/teste/test.py
+++ Projeto_2/teste/test.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../src')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../scr')
 
 from carro import Carro
 from cliente import Cliente
