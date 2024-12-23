942d995fed454113395e83251797c31a1fa8846d
Larissa <145572792+LarissaVie@users.noreply.github.com>
2024-12-23 11:29:41 -0300

    Add files via upload

---
+++ Projeto_3/Projeto_3/src/Pessoa.h
@@ -0,0 +1,27 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <string>
+#include <iostream>
+
+class Pessoa {
+private:
+    std::string nome;
+    std::string telefone;
+
+public:
+    // Construtor padrão
+    Pessoa();
+
+    // Construtor parametrizado
+    Pessoa(const std::string &nome, const std::string &telefone);
+
+    // Destrutor
+    ~Pessoa();
+
+    // Métodos de impressão
+    void imprimirNome() const;
+    void imprimirTelefone() const;
+};
+
+#endif // PESSOA_H
