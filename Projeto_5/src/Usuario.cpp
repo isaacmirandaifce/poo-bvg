67a20027c8ca7d6e3bef9008ed6c0dde7492ba43
Jordana dos Reis <jordanadosreis02@gmail.com>
2025-01-21 23:25:42 -0300

    Delete Projeto_5 directory

--- Projeto_5/src/Usuario.cpp
+++
@@ -1,6 +0,0 @@
-#include "Usuario.h"
-
-Usuario::Usuario() : nome(""), email(""), tipo("") {}
-
-Usuario::Usuario(const std::string& nome, const std::string& email, const std::string& tipo)
-    : nome(nome), email(email), tipo(tipo) {}
