a1fcba188f0b7603a47cf8939f1a87394756ed2d
alanlima0 <alanlima05022003@gmail.com>
2025-01-24 21:39:47 -0300

    Revert "adicao do projeto_6 versao 1.0"
    This reverts commit 166c437ba425201ff86f378746dbbfe52861e95c.

--- Projeto_6/src/models/TipoUsuario.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class TipoUsuario(Enum):
-    ALUNO = "Aluno"
-    PROFESSOR = "Professor"
-    FUNCIONARIO_ADMINISTRATIVO = "Funcionário Administrativo"
-    MONITOR = "Monitor"
-    DEFAULT = "indefinido"
