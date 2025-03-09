f751338cfeabea3cf548e7f6dc985062e0a84f52
Paulo Victor <paulovictorbezerrabrito23@gmail.com>
2025-03-09 20:32:11 -0300

    Add files via upload

---
+++ Projeto_7/Projeto_7-Paulo_Victor/include/Professor.h
@@ -0,0 +1,26 @@
+#ifndef PROFESSOR_H
+#define PROFESSOR_H
+
+#include "UsuarioAutenticavel.h"
+#include "Relatorio.h"
+#include "TipoUsuario.h"
+#include <string>
+#include <vector>
+
+class Professor : public UsuarioAutenticavel, public Relatorio {
+private:
+    std::string nome;
+    std::string areaDeAtuacao;
+    TipoUsuario tipo;
+    std::vector<std::string> disciplinasMinistradas;
+
+public:
+    Professor(std::string nome, std::string areaDeAtuacao, std::string senha);
+    bool autenticar(std::string senha) const override;
+    void gerarRelatorio() const override;
+    void adicionarDisciplinaMinistrada(std::string disciplina);
+    std::string getDisciplina() const;
+    void exibirInformacoes() const; 
+};
+
+#endif
---
+++ Projeto_7/Projeto_7-Paulo_Victor/include/Relatorio.h
@@ -0,0 +1,9 @@
+#ifndef RELATORIO_H
+#define RELATORIO_H
+
+class Relatorio {
+public:
+    virtual void gerarRelatorio() const = 0; 
+};
+
+#endif 
