cb32601899137e7e7e52c457a99dc2b63d29d53d
Gabriel Alves <124287240+Katsuo28@users.noreply.github.com>
2025-05-12 23:10:32 -0300

    Projeto_1-Gabriel Alves de Sales

---
+++ Projeto_1/Projeto_1-Gabriel_Alves/Test/test.py
@@ -0,0 +1,11 @@
+# Criando uma instância com dados diferentes
+cliente1 = Cliente("Carlos Andrade", 28, 750.0)
+
+# Exibindo informações iniciais
+cliente1.mostrar_informacoes()
+
+# Atualizando o saldo
+cliente1.atualizar_saldo(250.0)
+
+# Exibindo informações após a atualização
+cliente1.mostrar_informacoes()
