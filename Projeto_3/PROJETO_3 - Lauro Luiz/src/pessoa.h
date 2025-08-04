5103a132279fc57e0f956b0af7f7c2c9e44b160a
Lauro Luiz <laurowd>
2025-08-04 10:20:26 -0300

    Entrega do Projeto 3

---
+++ Projeto_3/PROJETO_3 - Lauro Luiz/src/pessoa.h
@@ -0,0 +1,20 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <string>
+
+class Pessoa {
+private:
+    std::string nome;
+    std::string telefone;
+
+public:
+    Pessoa();  
+    Pessoa(const std::string& nome, const std::string& telefone); 
+    ~Pessoa(); 
+
+    void imprimirNome() const;
+    void imprimirTelefone() const;
+};
+
+#endif
