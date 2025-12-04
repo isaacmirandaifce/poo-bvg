5b8145278fc80ff98a7abde93ad6f2dd5f80b485
Jhone <128087416+Jhone-rdz@users.noreply.github.com>
2025-12-04 22:16:06 +0000

    retirada de Projetos Anteriores

--- Projeto_1/tests/test.py
+++
@@ -1,9 +0,0 @@
-import sys, pathlib
-sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1]))
-from src.projeto_um import Cliente
-
-cliente1 = Cliente("João Silva", 30, 1000.0)
-cliente1.mostrar_informacoes()
-cliente1.atualizar_saldo(500.0)
-
-cliente1.mostrar_informacoes()
