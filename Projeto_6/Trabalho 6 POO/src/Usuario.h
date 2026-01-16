f7ad953a072025212aa94fb908273e5fbba35b19
Francisco <vieira.assis08@aluno.ifce.edu.br>
2026-01-15 21:10:09 -0300

    POO Trabalho 6

---
+++ Projeto_6/Trabalho 6 POO/src/Usuario.h
@@ -0,0 +1,23 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include <string>
+#include "TipoUsuario.h"
+
+class Usuario {
+protected:
+    std::string nome;
+    std::string email;
+    TipoUsuario tipo;
+
+public:
+    Usuario();
+    Usuario(const std::string& nome, const std::string& email, TipoUsuario tipo);
+    virtual ~Usuario();
+
+    const std::string& getNome() const;
+    const std::string& getEmail() const;
+    TipoUsuario getTipo() const;
+};
+
+#endif // USUARIO_H
