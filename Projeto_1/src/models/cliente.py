03580158c5e8c7da73e58ae637653fd2e9fc22ab
Ivamilton <ivamiltonjunior@gmail.com>
2026-04-06 11:16:03 -0300

    feat: adição de getters com @property

--- Projeto_1/src/models/cliente.py
+++ Projeto_1/src/models/cliente.py
@@ -4,3 +4,20 @@ class  Cliente:
         self.__idade = idade
         self.__saldo = saldo
         self.__ativo = True
+
+    @property
+    def nome(self):
+        return self.__nome
+    @property
+    def idade(self):
+        return self.__idade
+    @property
+    def saldo(self):
+        return self.__saldo
+    @property
+    def ativo(self):
+        return self.__ativo
+
+
+
+            
