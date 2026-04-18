6303a41d5cc2124e7c91c47df05253de4fecbf02
Dan Mendes <danzitomendesnoguera@hotmail.com>
2026-04-18 19:08:41 -0300

    Mudança de nome em arquivo de funções.hpp para funcoes.hpp. Adição das funções em funcoes.hpp.

---
+++ Projeto_2/Projeto_2 - Dan Mendes de Souza Nogueira Ribeiro/src/models/funcoes.hpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include "cliente.cc"
+#include "seguros.cc"
+
+void adicionarInformaçõesClientes(std::string nome, std::string cpf, cliente clienteAdici)
+{
+     
+    clienteAdici.setNome(nome);
+    clienteAdici.setCPF(cpf);
+};
+
+void adicionarInformaçõesSegurosGeral(double base, seguro seguroSetar)
+{
+    if (base < 0)
+    {
+        std::cout << "Erro! Valor Inadequado" << '\n';
+        return;
+    } else 
+    {
+        seguroSetar.setBase(base);
+        return;
+    }
+};
+
+void adicionarInformaçõesSegurosAuto(int ano, std::string placa, seguroAuto seguroSetar)
+{
+    if (ano < 0)
+    {
+        std::cout << "Erro! Valor Inadequado em ano" << '\n';
+        return;
+    }
+    else
+    {
+        seguroSetar.setAno(ano);
+        seguroSetar.setPlacaCarro(placa);
+        return;
+    }
+};
+
+void adicionarInformaçõesSegurosVida(int idade, std::string causa, seguroVida seguroSetar)
+{
+    if (idade < 0)
+    {
+        std::cout << "Erro! Valor Inadequado em idade" << '\n';
+        return;
+    }
+    else
+    {
+        seguroSetar.setIdade(idade);
+        seguroSetar.setCausa(causa);
+        return;
+    }
+};
+
+void adicionarInformaçõesSegurosResidencial(std::string tipoImovel, std::string endereco, seguroResidencial seguroSetar)
+{
+    seguroSetar.setTipoImovel(tipoImovel);
+    seguroSetar.setEndereco(endereco);
+};
