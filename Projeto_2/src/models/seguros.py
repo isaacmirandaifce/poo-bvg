327fb2b551db922e418f25723c7e7cd212653211
Emyyliano <emylianocarvalhocosta@gmail.com>
2026-04-28 09:07:41 -0300

    SEGURO IMOVEL

--- Projeto_2/src/models/seguros.py
+++ Projeto_2/src/models/seguros.py
@@ -4,7 +4,7 @@ from models.cliente import cliente
 class seguro(ABC):
     def __init__(self, titular: cliente, valorBase: float):
         self.__titularCarro = titular
-        self.__valorCarro = valorBase
+        self.__valor = valorBase
 
     @property
     def titular(self) -> cliente:
@@ -12,33 +12,46 @@ class seguro(ABC):
     
     @property
     def valor(self) -> float:
-        return self.__valorCarro
+        return self.__valor
     
     @abstractmethod
     def calcular_premio(self) -> float:
         pass
 
 class seguroAutomovel(seguro):
-    def __init__(self, titular: cliente, valorCarro: float, anoVeiculo: int, placaVeiculo: str):
-        super.__init__(titular, valorCarro)
+    def __init__(self, titular: cliente, valor: float, anoVeiculo: int, placaVeiculo: str):
+        super.__init__(titular, valor)
         self.__anoCarro = anoVeiculo
         self.__placaCarro = placaVeiculo
 
     def calcular_premio(self) -> float:
         if self.__anoCarro < 2010:
-            return self.__valorCarro * 1.2
+            return self.__valor * 1.2
         else:
-            return self.__valorCarro * 1.05
+            return self.__valor * 1.05
         
 class seguroVida(seguro):
-    def __init__(self, titular: cliente, valorCarro: float, idadeCliente: int):
-        super.__init__(titular, valorCarro)
+    def __init__(self, titular: cliente, valor: float, idadeCliente: int):
+        super.__init__(titular, valor)
         self.__idade = idadeCliente
 
     def calcular_premio(self) -> float:
         if self.__idade > 60:
-            return self.__valorCarro * 2
+            return self.__valor * 2
         else:
-            return self.__valorCarro * 1.1
+            return self.__valor * 1.1
+        
+class seguroImovel(seguro):
+    def __init__(self, titular: cliente, valor: float, tipoImovel: str):
+        super.__init__(titular, valor)
+        self.__imovel = tipoImovel
+
+    def calcular_premio(self) -> float:
+        if self.__imovel == "casa":
+            return self.__valor * 1.15
+        else:
+            return self.__valor * 1.06
+        
+
 
         
