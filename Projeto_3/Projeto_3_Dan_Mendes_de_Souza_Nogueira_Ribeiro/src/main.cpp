868f4de94946b121e4b15df74146c14ee94f66c8
Dan Mendes <danzitomendesnoguera@hotmail.com>
2026-05-10 23:26:06 -0300

    Criação da main.cpp - Adicionando os contatos no vector com o push_back e fazendo o loop requisitados.

---
+++ Projeto_3/Projeto_3_Dan_Mendes_de_Souza_Nogueira_Ribeiro/src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "contato.h"
+
+
+int main () {
+
+    std::vector<Contato> contatos;
+
+    contatos.push_back(AdicionarContatos("Fulano", "(88) 9999-991"));
+
+    contatos.push_back(AdicionarContatos("Fulana", "(88) 9999-992"));
+
+    contatos.push_back(AdicionarContatos("Julano", "(88) 9999-993"));
+
+    for (int i = 0; i < contatos.size(); i++)
+        {
+            std::cout << "\nContato " << i + 1 << ":\n";
+            contatos.at(i).imprimirNome();
+            contatos.at(i).imprimirTelefone();
+            std::cout << '\n';
+        }
+
+    return 0;
+}
