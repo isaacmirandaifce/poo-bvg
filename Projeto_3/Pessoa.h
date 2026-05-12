73488f649692100a75d2a42d5601f02d03d431c5
brenosousaa <147402159+brenosousaa@users.noreply.github.com>
2026-05-11 23:03:40 -0300

    Projeto 3 C++

---
+++ Projeto_3/Pessoa.h
@@ -0,0 +1,29 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Pessoa {
+private:
+    string nome;
+    string telefone;
+
+public:
+    // Construtor padrão
+    Pessoa();
+
+    // Construtor parametrizado
+    Pessoa(string nome, string telefone);
+
+    // Destrutor
+    ~Pessoa();
+
+    // Métodos de impressão
+    void imprimirNome();
+    void imprimirTelefone();
+};
+
+#endif // PESSOA_H
