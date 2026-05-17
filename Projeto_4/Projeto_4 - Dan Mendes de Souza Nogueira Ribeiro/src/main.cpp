50fb2d3d4ea249d9f6d8234581bab1e1a9462d30
Dan Mendes <danzitomendesnoguera@hotmail.com>
2026-05-16 23:13:29 -0300

    Criação de teste da main.

---
+++ Projeto_4/Projeto_4 - Dan Mendes de Souza Nogueira Ribeiro/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "transacao.h"
+#include "contaBancaria.h"
+
+
+int main () {
+
+    contaBancaria conta;
+
+    conta.setTitular("Fulano");
+
+    conta.setCPF("000.000.000-00");
+
+    conta.setSaldo(150);
+
+
+    Transacao novaTransacao;
+
+    novaTransacao.setCPF("000.000.000-00");
+    
+    novaTransacao.setValor(200);
+
+    validarTransacao(novaTransacao, conta);
+
+    return 0;
+}
