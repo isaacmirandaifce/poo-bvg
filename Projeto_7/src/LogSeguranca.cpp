4dc87bb4801acd0e4110d7fe1fcc811abcc0b4c9
João Lucas <savageluccx@gmail.com>
2026-05-23 12:29:20 -0300

    Header e constructor de LogSeguranca criado.

---
+++ Projeto_7/src/LogSeguranca.cpp
@@ -0,0 +1,15 @@
+#include "LogSeguranca.h"
+
+LogSeguranca::LogSeguranca(std::string timestamp, std::string nivel, std::string mensagem) : timestamp(timestamp), nivel(nivel), mensagem(mensagem) {}
+
+std::string LogSeguranca::getTimestamp() const {
+    return timestamp; 
+}
+
+std::string LogSeguranca::getNivel() const {
+    return nivel;
+}
+
+std::string LogSeguranca::getMensagem() const {
+    return mensagem;
+}
