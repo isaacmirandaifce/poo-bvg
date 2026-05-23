bb75dc7660bf92ac35c04cca77f22887e3dc4c3c
João Lucas <savageluccx@gmail.com>
2026-05-23 12:29:40 -0300

    Header e constructor de Transacao criado.

---
+++ Projeto_7/src/Transacao.cpp
@@ -0,0 +1,15 @@
+#include "Transacao.h"
+
+Transacao::Transacao(int id, double valor, std::string tipo) : id(id), valor(valor), tipo(tipo) {}
+
+int Transacao::getId() const {
+    return id;
+}
+
+double Transacao::getValor() const {
+    return valor;
+}
+
+std::string Transacao::getTipo() const {
+    return tipo;
+}
