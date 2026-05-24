9a19604ae49632e31ff450736a6421e6f1715756
Dan Mendes <danzitomendesnoguera@hotmail.com>
2026-05-24 20:29:18 -0300

    Inserido o código protótipo nos arquivos transacao.cpp e transacao.h

---
+++ Projeto_7/Projeto_7 - Dan Mendes de Souza Nogueira Ribeiro/scr/transacao.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include "transacao.h"
+
+void Transacao::setID(int id){
+    if (id>0)
+    {
+        this->id = id;
+    } else
+    {
+        std::cout << std::endl << "erro no ID" << std::endl;
+    }
+    
+};
+void Transacao::setValor(double valor){
+    if (valor >= 0)
+    {
+        this->valor = valor;
+    }
+    else
+    {
+        std::cout << std::endl << "erro no valor" << std::endl;
+    }
+};
+void Transacao::setTipo(std::string tipo){
+    if (tipo != "")
+    {
+        this->tipo = tipo;
+    }
+    else
+    {
+        std::cout << std::endl
+                  << "erro no tipo" << std::endl;
+    }
+};
+
+int Transacao::getID(){ return this->id;};
+double Transacao::getValor() { return this->valor; };
+std::string Transacao::getTipo(std::string tipo) { return this->tipo;};
---
+++ Projeto_7/Projeto_7 - Dan Mendes de Souza Nogueira Ribeiro/scr/transacao.h
@@ -0,0 +1,24 @@
+#ifndef transcacao
+#define transcacao
+#include <iostream>
+#include <string>
+
+
+class Transacao {
+    private:
+        int id;
+        double valor;
+        std::string tipo;
+
+    public:
+
+    void setID(int id);
+    void setValor(double valor);
+    void setTipo(std::string tipo);
+    
+    int getID();
+    double getValor();
+    std::string getTipo(std::string tipo);
+};
+
+#endif
