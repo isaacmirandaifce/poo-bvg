09cc5380d2199fc05c30a942e8100cd369b47b31
Dan Mendes <danzitomendesnoguera@hotmail.com>
2026-06-07 02:17:49 -0300

    Correção do código e acrescentando a sSobrecarga de métodos.

--- Projeto_5/Projeto_5 - Dan Mendes de Souza Nogueira Ribeiro/src/main.cpp
+++ Projeto_5/Projeto_5 - Dan Mendes de Souza Nogueira Ribeiro/src/main.cpp
@@ -9,17 +9,38 @@ int main(){
 
     std::vector<Dispositivo *> vetorDispositivo;
 
-    vetorDispositivo.push_back(new SensorDiagnostico);
+    SensorDiagnostico* sensorDia = new SensorDiagnostico;
 
-    vetorDispositivo.push_back(new SensorGPS);
+    sensorDia->setRPMMotor(6100);
+    sensorDia->setTemperaturaFluido(80.00);
+    vetorDispositivo.push_back(sensorDia);
 
-    vetorDispositivo.push_back(new RastreadorAvancado);
+    SensorGPS *sensorGps = new SensorGPS;
+
+    sensorGps->setLatitude(-900.00);
+    sensorGps->setLongitude(-800.00);
+    vetorDispositivo.push_back(sensorGps);
+
+    RastreadorAvancado *rasAvancado = new RastreadorAvancado();
+    rasAvancado->setRPMMotor(5500);
+    rasAvancado->setTemperaturaFluido(92.2);
+    rasAvancado->setLatitude(550.51);
+    rasAvancado->setLongitude(-124.00);
+    vetorDispositivo.push_back(rasAvancado);
 
     for (int i = 0; i < vetorDispositivo.size(); i++)
     {
 
         vetorDispositivo[i]->processarDados();
     }
+
+    std::cout << std::endl;
+
+    SensorGPS testeGPS;
+
+    testeGPS.transmitirPayload();
+
+    testeGPS.transmitirPayload("CHAVETESTE");
 
     for (int i = 0; i < vetorDispositivo.size(); i++)
     {
