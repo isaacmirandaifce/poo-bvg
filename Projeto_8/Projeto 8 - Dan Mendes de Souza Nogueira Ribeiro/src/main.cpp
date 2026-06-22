f2caa1e2cf887a99fc887dafa36b541e58db73ca
Dan Mendes <danzitomendesnoguera@hotmail.com>
2026-06-21 23:02:11 -0300

    Implementação do Código Base sugerido.

---
+++ Projeto_8/Projeto 8 - Dan Mendes de Souza Nogueira Ribeiro/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "infrastructure/ledgerPersistence.h"
+#include "infrastructure/signalHandler.h"
+#include "exceptions/storageException.h"
+
+int main()
+{
+    SignalHandler::inicializar(); // Registra os hooks de sinal do SO
+
+    try
+    {
+        LedgerPersistence db;
+        // 1. Simular carregamento inicial (pode disparar FileCorruptedException)
+        auto historico = db.carregarDados();
+
+        // 2. Loop de processamento de transações (simulação)
+        std::cout << "[ENGINE] Sistema operacional e aguardando interceptações..." << std::endl;
+
+        // CÓDIGO DO ALUNO: Adicione lógica de simulação e persistência aqui...
+    }
+    catch (const StorageException &e)
+    {
+        std::cerr << "[CRITICAL ERROR] Falha na camada de armazenamento: " << e.what() << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "[UNKNOWN ERROR] Erro genérico de runtime: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
