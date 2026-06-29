fda34b118bc9ad3d2aafdbc24875088d5a1e81ac
João Lucas <savageluccx@gmail.com>
2026-06-29 11:16:29 -0300

    Header de <<LedgerPersistence>> criado. Com métodos de salvar e carregar dados.

---
+++ Projeto_8/src/infrastructure/LedgerPersistence.h
@@ -0,0 +1,13 @@
+#ifndef LEDGERPERSISTENCE_H
+#define LEDGERPERSISTENCE_H
+
+#include <vector>
+#include <string>
+
+class LedgerPersistence {
+    public:
+        void salvarDados(const std::vector<std::string>& transacoes);
+        std::vector<std::string> carregarDados();
+};
+
+#endif
