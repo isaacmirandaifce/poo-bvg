715094b953b94b3deba3ad611c41f588684e0e6b
Emyyliano <emylianocarvalhocosta@gmail.com>
2026-08-02 16:30:38 -0300

    Fixando arquivos ( Projeto 2 )
    Reenviando os projetos para organização

---
+++ Projeto_2/src/models/seguros.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from models.cliente import cliente
+
+class seguro(ABC):
+    def __init__(self, titular: cliente, valorBase: float):
+        self.__titularCarro = titular
+        self.__valor = valorBase
+
+    @property
+    def titular(self) -> cliente:
+        return self.__titularCarro
+    
+    @property
+    def valor(self) -> float:
+        return self.__valor
+    
+    @abstractmethod
+    def calcular_premio(self) -> float:
+        pass
+
+class seguroAutomovel(seguro):
+    def __init__(self, titular: cliente, valor: float, anoVeiculo: int, placaVeiculo: str):
+        super().__init__(titular, valor)
+        self.__anoCarro = anoVeiculo
+        self.__placaCarro = placaVeiculo
+
+    def calcular_premio(self) -> float:
+        if self.__anoCarro < 2010:
+            return self.valor * 1.2
+        else:
+            return self.valor * 1.05
+        
+class seguroVida(seguro):
+    def __init__(self, titular: cliente, valor: float, idadeCliente: int):
+        super().__init__(titular, valor)
+        self.__idade = idadeCliente
+
+    def calcular_premio(self) -> float:
+        if self.__idade > 60:
+            return self.valor * 2
+        else:
+            return self.valor * 1.1
+        
+class seguroImovel(seguro):
+    def __init__(self, titular: cliente, valor: float, tipoImovel: str):
+        super().__init__(titular, valor)
+        self.__imovel = tipoImovel
+
+    def calcular_premio(self) -> float:
+        if self.__imovel == "casa":
+            return self.valor * 1.15
+        else:
+            return self.valor * 1.06
+        
+
+
+        
