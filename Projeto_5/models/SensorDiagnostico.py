d23ba38c4401f5bad0fddf2eaa79f121e08ddef7
Emyyliano <emylianocarvalhocosta@gmail.com>
2026-08-02 23:31:19 -0300

    Fix Sensor do Motor

---
+++ Projeto_5/models/SensorDiagnostico.py
@@ -0,0 +1,10 @@
+from models.Dispositivo import Dispositivo
+
+class SensorDiagnostico(Dispositivo):
+    def __init__(self, id_dispositivo: str, timestamp: int, rpm_motor: int, temperatura_fluido: float):
+        Dispositivo.__init__(self, id_dispositivo, timestamp)
+        self.__rpmMotor = rpm_motor                  
+        self.__temperaturaFluido = temperatura_fluido  
+
+    def processarDados(self) -> None:
+        print(f"[OBD-II] Processando {self._idDispositivo} | RPM: {self.__rpmMotor} | Temp: {self.__temperaturaFluido}C")
