cdacf25dcd20f6c8a7ef8df144abc922f3850f6c
Emyyliano <emylianocarvalhocosta@gmail.com>
2026-08-02 23:34:10 -0300

    Fix Correção dos nomes do arquivos

---
+++ Projeto_5/models/SensorGPS.py
@@ -0,0 +1,16 @@
+from models.Dispositivo import Dispositivo
+
+class SensorGPS(Dispositivo):
+    def __init__(self, id_dispositivo: str, timestamp: int, latitude: float, longitude: float):
+        Dispositivo.__init__(self, id_dispositivo, timestamp)
+        self.__latitude = latitude    
+        self.__longitude = longitude  
+
+    def processarDados(self) -> None:
+        print(f"GPS Processando {self._idDispositivo} | Lat: {self.__latitude}, Lon: {self.__longitude}")
+
+    def transmitirPayload(self, chave_cripto: str = None) -> None:
+        if chave_cripto is None:
+            print(" -> Enviando payload em texto puro (Inseguro).")
+        else:
+            print(f" -> Enviando payload criptografado com chave [{chave_cripto}].")
